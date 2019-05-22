b92a4686cca540a7c55b3491d2a47086c019782a
JIH <ford6313@gmail.com>
2019-05-22 11:19:32 +0900

    url 연결

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import ttk
-import tkinter.messagebox
+from urllib.parse import urlparse
+from urllib.request import urlopen,Request
+from urllib.parse import urlencode, quote_plus
+from xml.dom.minidom import parseString
+
 
 
 class App:
@@ -28,19 +32,35 @@ class App:
 
 
 
-
         self.input = Entry(frameSearch, width=30)
         self.input.pack(side=LEFT)
         Button(frameSearch,text='검색',command=self.Search).pack()
-        scrollbar = Scrollbar(win)
-        self.txtBox = Text(frameList, width=40,yscrollcommand=scrollbar.set)
-        self.txtBox.pack(fill=BOTH)
-        scrollbar.config(command=self.txtBox.yview)
-        #scrollbar.grid(row=2,column=1)
+        scrollbar = Scrollbar(frameList)
+        self.listBox = Listbox(frameList,selectmode='extended', width=40,height=20, yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=RIGHT,fill=BOTH)
+        self.listBox.pack(side=LEFT)
+        scrollbar.config(command=self.listBox.yview)
+
 
         win.mainloop()
 
     def Search(self):
-        pass
+        url= 'http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getKwrdFndSubwaySttnList'
+        queryParams = '?' + urlencode({quote_plus('ServiceKey'): 'e20GlP6AHkpkkdAr0AYT50r6zfv%2Fgj8KNbomL7RzhiSCSxpFb0vhZgYU7DADHoto16Zxg7xK01%2BCd69yoAssag%3D%3D', quote_plus('subwayStationName'): '서울'})
+
+        req = Request(url+queryParams)
+        req.get_method = lambda: 'GET'
+        res_body = urlopen(req).read()
+        print(res_body)
+        xmldata = parseString(res_body)
+
+        self.listBox.delete(0, int(self.listBox.size())-1)
+
+        list = xmldata.childNodes
+        #stops = list[1].childNodes
+        #for s in stops:
+        #    if s.nodeName == 'item':
+        #        self.listBox.insert(0, s.firstChild.nodeValue)
+
 
 App()
