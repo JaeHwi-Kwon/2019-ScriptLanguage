c0be24916693691857d5f0e961067ed8fc1b82bf
teronalision <teronalision@gmail.com>
2019-05-25 21:38:03 +0900

    검색 완성

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ class App:
         win.geometry("800x600")
         win.resizable(False, False)
 
-
+        #전체 틀 초기화
         Tap = ttk.Notebook(win, width=500,height=600)
         Tap.pack(side=RIGHT)
         frameLeft = Frame(win)
@@ -34,22 +34,25 @@ class App:
         Tap.add(frameTime, text='   시간표   ')
 
 
-
+        #왼쪽 프레임
         self.input = Entry(frameSearch, width=30)
         self.input.pack(side=LEFT)
+        self.input.bind('<Return>', self.Search)
         Button(frameSearch,text='검색',command=self.Search).pack()
         scrollbar = Scrollbar(frameList)
-        self.listBox = Listbox(frameList,selectmode='extended', width=40,height=20, yscrollcommand=scrollbar.set)
+        self.listBox = Listbox(frameList,selectmode='extended', width=40,height=20,yscrollcommand=scrollbar.set)
         scrollbar.pack(side=RIGHT,fill=BOTH)
         self.listBox.pack(side=LEFT)
+        self.listBox.bind('<Double-Button-1>',self.SelectList)
         scrollbar.config(command=self.listBox.yview)
 
-        self.xmldata = None
+
+        self.stationList = []
 
 
         win.mainloop()
 
-    def Search(self):
+    def Search(self, event=None):   #역이름 검색
         keyword = self.input.get()
         key = 'e20GlP6AHkpkkdAr0AYT50r6zfv%2Fgj8KNbomL7RzhiSCSxpFb0vhZgYU7DADHoto16Zxg7xK01%2BCd69yoAssag%3D%3D'
         url = 'http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getKwrdFndSubwaySttnList'
@@ -62,11 +65,16 @@ class App:
         dataTree = ElementTree.fromstring(res_body)
 
 
-        self.listBox.delete(0, int(self.listBox.size())-1)
+        self.listBox.delete(0, END)
         namelist = dataTree.getiterator('item')
         for item in namelist:
             name = item.findtext('subwayStationName')
             self.listBox.insert(END, name)
+            self.stationList.append(name)
 
+    def SelectList(self, event):    #목록 더블클릭 했을때
+        a = self.listBox.curselection()[0]
+        print(self.stationList[a])
+        #여기다 오른쪽 탭 갱신하는 함수 호출하면 돼
 
 App()
