f20fb447ff573bdb46e146c3acbb6a215b3124a4
2DGP <kegedokoota@naver.com>
2019-06-17 06:27:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 
 setup(name='sub', version='0.7',
-      py_modules=['test','Map','NaverMaps','NaverSearch','telegram','TimeTalbe'],
-      package_data=['bak.png','Linemap.jpg','Title.png']
+      py_modules=['test', 'Map', 'NaverMaps', 'NaverSearch', 'telegram', 'TimeTable', 'Graph'],
+      package_data=['bak.png', 'Linemap.jpg', 'Title.png']
       )
